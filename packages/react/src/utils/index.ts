2cd970474d0ed07d1aa012c13151ec1f7c0bedf9
xjw mac <xjw@mac.com>
2026-08-31 21:24:02 +0800

    refactor: remove tv utility and update sidebar styles for overflow handling

--- packages/react/src/utils/index.ts
+++ packages/react/src/utils/index.ts
@@ -7,5 +7,3 @@ export * from "./use-has-text-slot";
 export * from "./logger";
 export * from "./dom";
 export * from "./variants";
-export {tv} from "./tv";
-export type {VariantProps} from "./tv";
